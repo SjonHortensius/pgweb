4171e973d2213d5ddea665e1e6b01f3f98d0e4de
Sjon Hortensius <sjon@hortensius.net>
2016-10-03 17:09:37 +0200

    Format rows_count by adding localized thousands separator

--- pkg/statements/sql.go
+++ pkg/statements/sql.go
@@ -70,7 +70,7 @@ SELECT
   pg_size_pretty(pg_table_size($1)) AS data_size,
   pg_size_pretty(pg_indexes_size($1)) AS index_size,
   pg_size_pretty(pg_total_relation_size($1)) AS total_size,
-  (SELECT reltuples FROM pg_class WHERE oid = $1::regclass) AS rows_count`
+  (SELECT to_char(reltuples, '9G999') FROM pg_class WHERE oid = $1::regclass) AS rows_count`
 
 	// ---------------------------------------------------------------------------
 
